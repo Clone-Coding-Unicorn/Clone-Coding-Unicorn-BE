f4439e0b6faad7d264f24d85c1b6dc1d4bdefdfc
Jisu Lee <jisulee.shsf@gmail.com>
2024-01-25 16:20:08 +0900

    fix: OAuth Google & kakao 로직 변경

--- src/main/java/com/sparta/clonecodingunicorn/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/clonecodingunicorn/domain/member/controller/MemberController.java
@@ -5,6 +5,8 @@ import com.sparta.clonecodingunicorn.domain.member.dto.SignupRequestDto;
 import com.sparta.clonecodingunicorn.domain.member.service.GoogleService;
 import com.sparta.clonecodingunicorn.domain.member.service.KakaoService;
 import com.sparta.clonecodingunicorn.domain.member.service.MemberService;
+import com.sparta.clonecodingunicorn.global.jwt.JwtUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +14,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
 @RequestMapping("/api")
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 public class MemberController {
     private final MemberService memberService;
-    private final KakaoService kakaoService;
     private final GoogleService googleService;
+    private final KakaoService kakaoService;
 
-    public MemberController(MemberService memberService, KakaoService kakaoService, GoogleService googleService) {
+    public MemberController(MemberService memberService, GoogleService googleService, KakaoService kakaoService) {
         this.memberService = memberService;
-        this.kakaoService = kakaoService;
         this.googleService = googleService;
+        this.kakaoService = kakaoService;
     }
 
     // 가입
@@ -32,15 +33,25 @@ public class MemberController {
         return memberService.signup(requestDto, bindingResult);
     }
 
-    // kakao 로그인
-    @GetMapping("/kakao/callback")
-    public ResponseEntity<String> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
-        return kakaoService.kakaoLogin(code, response);
-    }
-
     // Google 로그인
     @GetMapping("/login/oauth2/code/google")
-    public ResponseEntity<String> googleLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
-        return googleService.googleLogin(code, response);
+    public String googleLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        String token = googleService.googleLogin(code);
+        String tokenValue = token.substring(7);
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, tokenValue);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
+
+    // kakao 로그인
+    @GetMapping("/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        String token = kakaoService.kakaoLogin(code);
+        String tokenValue = token.substring(7);
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, tokenValue);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
